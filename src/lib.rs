75bdacb0df4b443681444e451002b29f84e61ede
Travis Long <tlong@mozilla.com>
2021-02-16 15:54:43 -0600

    WIP, but it builds and has the symbols in the .a

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,16 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+#![allow(unknown_lints)]
+#![warn(rust_2018_idioms)]
+
+pub use autofill;
+pub use fxaclient_ffi;
+pub use logins_ffi;
+pub use nimbus;
+pub use places_ffi;
+pub use push_ffi;
+pub use rc_log_ffi;
+pub use sync_manager_ffi;
+pub use viaduct;
+pub use glean_ffi;
